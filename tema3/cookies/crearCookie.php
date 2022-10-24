7df759c0c659b8ab00ab55931d2f9c88faa61d94
jjguillen <jjavierguillen@gmail.com>
2022-10-24 19:16:45 +0200

    contadorVisitas

--- tema3/cookies/crearCookie.php
+++ tema3/cookies/crearCookie.php
@@ -10,17 +10,16 @@ if ($_GET) {
         //Aquí desencriptas los datos
         //-----
 
-        //Aquí habría que meter antes el contador de visitas.
-
         //Separar los gustos y meterlos en un array
         $gustosArray = explode("#",$gustos);
 
-        //CreacionCookie#moda-1#deportes-2
-        $encontrado = false;
+        //CreacionCookie # moda-2 # deportes-2  # juegos-0
+
         for($i=1; $i<count($gustosArray); $i++) {
             //Separa moda de 1
             
             $gustoContadorArray = explode("-",$gustosArray[$i]);
+            //Separamos por un lado moda (posición 0) y por otro el contador (posición 1)
 
             if ($_GET['interes'] == $gustoContadorArray[0]) {
                 $gustoContadorArray[1]++;
@@ -32,12 +31,14 @@ if ($_GET) {
         //Volvemos a convertir a string ya quitados los duplicados
         $gustosString = implode("#", $gustosArray);
         
-        
         //Aquí encriptas los datos 
         //-----
+
+        //Creación de la cookie
         setcookie('servidor',$gustosString, time()+60000, "/tema3", "localhost", false, true);
         //echo "Cookie creada";
     } else {
+        //Primera vez que entra
         setcookie('servidor',"CreacionCookie#moda-0#deporte-0#juegos-0", time()+60000, "/tema3", "localhost", false, true);
     }
 
