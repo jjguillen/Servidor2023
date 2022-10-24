36ebc31656f21db5cccd6e126a3f4cb32c6b8bdd
jjguillen <jjavierguillen@gmail.com>
2022-10-24 18:30:12 +0200

    cookies

--- tema3/cookies/crearCookie.php
+++ tema3/cookies/crearCookie.php
@@ -3,28 +3,32 @@
 //Si he pinchado en un link
 if ($_GET) {
 
-    //Leemos lo que ya te gusta
-    $gustos = $_COOKIE['servidor'];
+    if (isset($_COOKIE["servidor"])) {
+        //Leemos lo que ya te gusta
+        $gustos = $_COOKIE['servidor'];
 
-    //Aquí desencriptas los datos
-    //-----
+        //Aquí desencriptas los datos
+        //-----
 
-    //Aquí habría que meter antes el contador de visitas.
-    //juegos-1#ropa-4
-    $gustos = $gustos."#".$_GET['interes'];
+        //Aquí habría que meter antes el contador de visitas.
+        //juegos-1#ropa-4
+        $gustos = $gustos."#".$_GET['interes'];
 
-    //Separar los gustos y meterlos en un array
-    $gustosArray = explode("#",$gustos);
-    $gustosArray = array_unique($gustosArray);
+        //Separar los gustos y meterlos en un array
+        $gustosArray = explode("#",$gustos);
+        $gustosArray = array_unique($gustosArray);
 
-    //Volvemos a convertir a string ya quitados los duplicados
-    $gustosString = implode("#", $gustosArray);
-    
-    //Aquí encriptas los datos 
-    //-----
+        //Volvemos a convertir a string ya quitados los duplicados
+        $gustosString = implode("#", $gustosArray);
+        
+        //Aquí encriptas los datos 
+        //-----
+        setcookie('servidor',$gustosString, time()+60000, "/tema3", "localhost", false, true);
+        //echo "Cookie creada";
+    } else {
+        setcookie('servidor',"PrimeraVez2", time()+60000, "/tema3", "localhost", false, true);
+    }
 
-    setcookie('servidor',$gustosString, time()+60, "/tema2", "localhost", false, true);
-    //echo "Cookie creada";
 
     header("Location: index.php");
 }
