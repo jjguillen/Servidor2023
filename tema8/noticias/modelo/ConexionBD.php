aef1da92a546979c7b364dd9e10e8bed9fef824e
jjguillen <jjavierguillen@gmail.com>
2023-01-09 19:30:03 +0100

    mongo atlas

--- tema8/noticias/modelo/ConexionBD.php
+++ tema8/noticias/modelo/ConexionBD.php
@@ -13,8 +13,8 @@
 
             try {
                 //CONEXIÓN A MONGODB CLOUD ATLAS. Comentar esta línea para conectar en local.
-                //$host = "mongodb+srv://admin:evhT1Hu8ZasF8llx@cluster0.qmwhh.mongodb.net/".$database."?retryWrites=true&w=majority";
-                $host = "mongodb://root:toor@mongo:27017/"; //MongoDB en Docker
+                $host = "mongodb+srv://admin:rYuEG2NUGn0hpl4o@cluster0.qmwhh.mongodb.net/?retryWrites=true&w=majority";
+                //$host = "mongodb://root:toor@mongo:27017/"; //MongoDB en Docker
                 self::$conexion = (new Client($host))->{$bd};
             } catch (Exception $e){
                 echo $e->getMessage();
